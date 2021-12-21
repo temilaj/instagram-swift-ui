3b25ccdee09ef163e5b7211ce4a3890902561043
temilaj <temilaj@hotmail.com>
2021-12-21 18:57:23 -0500

    Initial Commit

---
+++ InstagramDemo/InstagramDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramDemoApp.swift
+//  InstagramDemo
+//
+//  Created by Temi Lajumoke on 12/21/21.
+//
+
+import SwiftUI
+
+@main
+struct InstagramDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
